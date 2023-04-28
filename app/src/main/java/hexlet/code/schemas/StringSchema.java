4b5ca7713fe37f36ecd97f1bed0826be875f4214
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-04-28 19:57:46 +0200

    fix: make heir-classes of BaseSchema class are final

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -2,7 +2,7 @@ package hexlet.code.schemas;
 
 import java.util.function.Predicate;
 
-public class StringSchema extends BaseSchema {
+public final class StringSchema extends BaseSchema {
 
     private String stringContent;
     private int minLength;
